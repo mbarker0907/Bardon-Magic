2e6c90a8c8adfc05212c5dc12d02eb8563923757
Michael Barker <mbarker0907@gmail.com>
2023-09-16 23:43:52 +0200

    restyled the element cards

--- bardon-next/components/ElementCard.js
+++ bardon-next/components/ElementCard.js
@@ -3,13 +3,23 @@ import { useSpring, animated } from 'react-spring';
 import styles from '../styles/elements.module.css';
 
 const ElementCard = ({ title, description, type }) => {
+  // State to track if the card is flipped
   const [flipped, setFlipped] = useState(false);
+
+  // State to track touch and hover events for scaling effect
   const [touching, setTouching] = useState(false);
   const [hovering, setHovering] = useState(false);
 
+  // Event handlers for mouse hover effects
   const handleMouseEnter = () => setHovering(true);
   const handleMouseLeave = () => setHovering(false);
 
+  // Event handler for card click
+  const handleCardClick = () => {
+    setFlipped(!flipped);
+  }
+
+  // Animation properties using react-spring
   const { transform, opacity } = useSpring({
     opacity: flipped ? 1 : 0,
     transform: `perspective(600px) rotateX(${flipped ? 180 : 0}deg)`,
@@ -19,7 +29,7 @@ const ElementCard = ({ title, description, type }) => {
   return (
     <div 
       className={`${styles.cardContainer} ${styles[type]}`} 
-      onClick={() => setFlipped(!flipped)}
+      onClick={handleCardClick}
       onMouseEnter={handleMouseEnter} 
       onMouseLeave={handleMouseLeave}
       onTouchStart={() => setTouching(true)}
--- bardon-next/pages/magic.js
+++ bardon-next/pages/magic.js
@@ -18,19 +18,19 @@ const Magic = () => {
               {/* Element Cards */}
               <div className={styles.cardsContainer}> 
     {/* Fire Card */}
-    <ElementCard type="fire" title="Fire" description="The element of passion, desire and energy." />
+    <ElementCard type="fire" title="Fire" description="Fire represents the principle of activity, transformation, and willpower. In Bardon's teachings, it's not just a physical flame but a symbol of our inner drive, passion, and the spark of our spirit. Mastery over the fire element allows the magician to harness their inner strength, overcome obstacles, and manifest their desires." />
 
     {/* Water Card */}
-    <ElementCard type="water" title="Water" description="The element of emotion, intuition and deep insight." />
+    <ElementCard type="water" title="Water" description="Water embodies the principle of fluidity, emotion, and intuition. It's the realm of feelings, dreams, and the subconscious mind. By understanding the water element, the magician learns to navigate their emotions, develop deep intuition, and connect with the astral plane." />
 
     {/* Air Card */}
-    <ElementCard type="air" title="Air" description="The element of intellect, logic and communication." />
+    <ElementCard type="air" title="Air" description="Air signifies the realm of the mind, intellect, and communication. It's about thoughts, ideas, and the bridges between people. Mastery over the air element empowers the magician with sharp intellect, clarity of thought, and effective communication skills." />
 
     {/* Earth Card */}
-    <ElementCard type="earth" title="Earth" description="The element of stability, foundation and physical sensation." />
+    <ElementCard type="earth" title="Earth" description="Earth stands for stability, materiality, and grounding. It's the foundation upon which all is built. By connecting with the earth element, the magician learns to stay grounded, manifest their ideas into reality, and understand the physical world's intricacies." />
 
     {/* Akasha Card */}
-    <ElementCard type="akasha" title="Akasha" description="The ethereal element representing spirit and space." />
+    <ElementCard type="akasha" title="Akasha" description="Akasha, or the ether, is the quintessence from which all elements arise. It's the binding force, the cosmic memory, and the space where all potentials exist. By tapping into the Akasha, the magician accesses the universal wisdom, understands past, present, and future, and realizes the interconnectedness of all things." />
 </div>
 
           </main>
